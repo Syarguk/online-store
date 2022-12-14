c47668ec608c38211f4045518970fa9d96447405
YazykovaDaria <yazykovadasva@gmail.com>
2022-12-14 22:31:20 +0300

    refactor: check settings

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import 'reset-css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.scss';
 
+const y = (n, d) => n + d;
 
-
-// const y = (n, d) => n + d
-
-// console.log(y(3, 5));
-console.log('hi');
+console.log(y(3, 5));
