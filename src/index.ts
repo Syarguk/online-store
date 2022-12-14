b8ed766d5eb995fb15fdf9a523ea818f3947220e
YazykovaDaria <yazykovadasva@gmail.com>
2022-12-14 12:25:56 +0300

    feat: add bootstrap, reset.css

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import './index.html';
+import 'reset-css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.scss';
 
 
 
-const y = (n, d) => n + d
+// const y = (n, d) => n + d
 
-console.log(y(3, 5));
+// console.log(y(3, 5));
 console.log('hi');
