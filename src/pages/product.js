489667b8d3de51b1973f56102a0f132c8492e2b0
YazykovaDaria <yazykovadasva@gmail.com>
2022-12-21 16:24:59 +0300

    feat: add base for build page

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,3 +1,14 @@
-const productPage = `<p>Product page</P>`;
+import { transformUrlToParams } from '../utillites/url';
+import model from '../model/model';
 
-export default productPage;
+export { transformUrlToParams } from '../utillites/url';
+
+const buildProductPage = (path) => {
+  const { id } = transformUrlToParams(path);
+  const product = model.getProduct(Number(id));
+  //тут делать страницу продукта
+  //console.log(product);
+  return `product page ${id}`;
+};
+
+export default buildProductPage;
