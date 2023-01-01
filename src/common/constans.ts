77e39be735be545d6efc1fea534a90c9ed52b1c2
YazykovaDaria <yazykovadasva@gmail.com>
2023-01-01 21:32:50 +0300

    feat: add filters

--- src/common/constans.ts
+++ src/common/constans.ts
@@ -1,1 +1,3 @@
-export const numbersTypeArea: string[] = ['id', 'price', 'discountPercentage', 'rating', 'stock']
+export const numbersTypeArea: string[] = ['id', 'price', 'discountPercentage', 'rating', 'stock'];
+
+export const filtersName = ['category', 'brand'];
