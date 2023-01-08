dd53be094afeb9dc90f9769bba986457b9581686
YazykovaDaria <yazykovadasva@gmail.com>
2023-01-08 20:12:17 +0300

    refactor: del comments

--- src/types/model.ts
+++ src/types/model.ts
@@ -2,19 +2,10 @@ import { Products, Product } from './products';
 
 interface Model {
   data: Products;
-  // filteredData: Products;
-  // isInit: boolean;
 
   getProducts(): Products;
   getProduct(productId: number): Product;
 
-  // getProductsForBasket(productsId: string[]): Products;
-  // //talk about this metods
-  // getFilterProducts(filter: string | null | undefined): void;
-  // getFilteredProducts(): Products;
-
-  // setFilterProducts(filter: string, value: string): void;
-  // dropFilterProducts(filter: string, value: string): void
 }
 
 export default Model;
