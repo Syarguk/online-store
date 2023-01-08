a7321eb151d34e8e6c67f3259b5e2055c3fb3069
YazykovaDaria <yazykovadasva@gmail.com>
2023-01-08 20:11:53 +0300

    refactor: del comments

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,14 +1,9 @@
-//import { Products } from '../types/products';
-// import { Product } from '../types/products';
 import { products } from './data';
 import Model from '../types/model';
 
 const model: Model = {
 
   data: products,
-  //dont need
-  // filteredData: [],
-  // isInit: false,
 
   getProducts() {
     return this.data;
@@ -19,39 +14,6 @@ const model: Model = {
     const [product] = filterProducts;
     return product;
   },
-
-  // getProductsForBasket(productsId) {
-  //   return this.data.filter((product) => productsId.includes(String(product.id)));
-  // },
-
-  // //dont need
-  // setFilterProducts(filter, value) {
-  //   const filterProd = this.data.filter((product) =>
-  //   // console.log(filter);
-
-  //     product[filter] === value);
-  //   console.log(filterProd);
-
-  //   this.filteredData = this.filteredData.concat(filterProd);
-
-  //   // console.log(this.filteredData);
-  // },
-
-  // dropFilterProducts(filter, value) {
-  //   const filterProd = this.filteredData.filter((product) => product[filter] !== value);
-  //   this.filteredData = filterProd;
-  //   // this.filteredData.concat(filterProd);
-  //   // console.log(this.filteredData);
-  // },
-
-  // getFilteredProducts() {
-  //   return this.filteredData;
-  // },
-
-  // getFilterProducts(filter) {
-  //   const filterProd = this.data.filter((product) => product.category === filter);
-  //   this.filteredData = this.filteredData.concat(filterProd);
-  // },
 
 };
 
